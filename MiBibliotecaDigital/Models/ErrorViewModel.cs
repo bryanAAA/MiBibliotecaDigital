b8123b9b7683b647dcf02430c1651d6b2e5474b0
bryanAAA <arroyob29@gmail.com>
2025-03-07 19:05:42 -0600

    Add project files.

---
+++ MiBibliotecaDigital/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiBibliotecaDigital.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
